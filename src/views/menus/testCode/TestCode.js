13bb2ff74fee3c941ee086a4900424c4f999ec36
MzaThomas <minzaw1682@gmail.com>
2023-04-30 13:40:21 +0630

    first push

---
+++ src/views/menus/testCode/TestCode.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import TestCodeComponent from "./TestCodeComponent";
+import TestCodeCom2 from "./TestCodeCom2";
+import Chart from 'react-apexcharts';
+import { CCard, CCardBody, CCardHeader } from '@coreui/react';
+
+const TestCode=()=>{
+
+  
+    return(
+        <div>
+            <TestCodeCom2 name="Mg Mg" age="12" address="Yangon"/>
+            <TestCodeComponent name="Sara"/>
+            <TestCodeComponent name="Nice"/>
+            <TestCodeCom2 name="Ma Ma" age="13" address="Singapore"/>           
+        </div>
+    )
+}
+
+export default TestCode;
